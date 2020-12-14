1592da2de0ece3e7a0a6a9fd32832025b95fb9c0
AndriyKuzmenko <59597557+AndriyKuzmenko@users.noreply.github.com>
2020-12-14 19:01:02 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/internalfiles/MainActivity.java
+++ app/src/main/java/com/example/internalfiles/MainActivity.java
@@ -4,25 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-
+    EditText eT;
+    TextView tV;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        tV=(TextView)findViewById(R.id.tV);
+        eT=(EditText)findViewById(R.id.eT);
     }
 
     public void save(View view)
     {
-
+        
     }
 
     public void reset(View view)
     {
-
+        
     }
 
     public void exit(View view)
