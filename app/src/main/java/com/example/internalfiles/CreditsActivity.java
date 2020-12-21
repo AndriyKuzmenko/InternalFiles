bd1c200bb536f42442535afe593a79ebc91a4989
AndriyKuzmenko <59597557+AndriyKuzmenko@users.noreply.github.com>
2020-12-21 17:42:44 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/internalfiles/CreditsActivity.java
@@ -0,0 +1,27 @@
+package com.example.internalfiles;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class CreditsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+
+    public void back(View view)
+    {
+        finish();
+    }
+}
