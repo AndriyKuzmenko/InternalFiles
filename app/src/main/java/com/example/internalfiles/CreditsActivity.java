9b64372f05a3885beeb223ae368adaf5f21d4782
AndriyKuzmenko <59597557+AndriyKuzmenko@users.noreply.github.com>
2020-12-23 17:03:49 +0200

    Update CreditsActivity.java

--- app/src/main/java/com/example/internalfiles/CreditsActivity.java
+++ app/src/main/java/com/example/internalfiles/CreditsActivity.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class CreditsActivity extends AppCompatActivity
 {
@@ -16,8 +17,34 @@ public class CreditsActivity extends AppCompatActivity
         setContentView(R.layout.activity_credits);
     }
 
-    public void back(View view)
+    /**
+     * @param menu  - the menu
+     * @return      creates a menu with two options - credits and main activity
+     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
     {
-        finish();
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    /**
+     *
+     * @param item - the item that was selected
+     * @return     After the user pressed on the main activity option, this method
+     *             goes back to main activity
+     */
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId()==R.id.menuCredits)
+        {
+            finish();
+        }
+
+        return true;
     }
 }
